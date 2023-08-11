45bb0893fc4e6b28f00e2514db932a58876de707
vantonioadm <vantonioadm@nellc365.onmicrosoft.com>
2023-08-10 22:01:43 -0500

    added bash commands for UI

--- CreateContainerResources.sh
+++ CreateContainerResources.sh
@@ -37,4 +37,22 @@ az group create \
   --registry-server $ACR_NAME.azurecr.io \
   --query properties.configuration.ingress.fqdn
 
+
+git clone https://github.com/$GITHUB_USERNAME/containerapps-albumui.git code-to-cloud-ui
+
+az acr build --registry $ACR_NAME --image albumapp-ui .
+
+API_BASE_URL=$(az containerapp show --resource-group $RESOURCE_GROUP --name $API_NAME --query properties.configuration.ingress.fqdn -o tsv)
+
+az containerapp create \
+  --name $FRONTEND_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --environment $ENVIRONMENT \
+  --image $ACR_NAME.azurecr.io/albumapp-ui  \
+  --target-port 3000 \
+  --env-vars API_BASE_URL=https://$API_BASE_URL \
+  --ingress 'external' \
+  --registry-server $ACR_NAME.azurecr.io \
+  --query properties.configuration.ingress.fqdn
+
   az group delete --name $RESOURCE_GROUP
